10033ab8cec942e28355e3f82510bd5dd64f041e
kamclean <mcleankaca@gmail.com>
2021-12-15 14:53:39 +0000

    data

---
+++ R/example_data.R
@@ -0,0 +1,33 @@
+# Documentation of datasets --------------------------------
+# example_probast
+#' @docType data
+#' @title Example dataset of PROBAST assessment
+#' @name example_probast
+#' @aliases example_probast
+#' @description Example dataset of PROBAST assessment for use within vignettes
+#' @format A data frame with 36 rows and 25 variables.
+#' @usage data(example_probast)
+#' @keywords data
+NULL
+
+# example_tripod
+#' @docType data
+#' @title Example dataset of TRIPOD assessment
+#' @name example_tripod
+#' @aliases example_tripod
+#' @description Example dataset of TRIPOD assessment for use within vignettes
+#' @format A data frame with 36 rows and 39 variables.
+#' @usage data(example_tripod)
+#' @keywords data
+NULL
+
+# example_prediction
+#' @docType data
+#' @title Example dataset of model predictions
+#' @name example_prediction
+#' @aliases example_prediction
+#' @description Example dataset of model predictions for use within vignettes
+#' @format A data frame with 600 rows and 3 variables.
+#' @usage data(example_prediction)
+#' @keywords data
+NULL
