022cbeb8aed413d7e5a2e7b16e5e77e8b7d44e5f
kamclean <mcleankaca@gmail.com>
2022-08-08 19:17:15 +0100

    .

--- data-raw/example_probast.R
+++
@@ -1,3 +0,0 @@
-## code to prepare `example_probast` dataset goes here
-
-usethis::use_data(example_probast, overwrite = TRUE)
--- data-raw/example_tripod.R
+++
@@ -1,3 +0,0 @@
-## code to prepare `example_tripod` dataset goes here
-
-usethis::use_data(example_tripod, overwrite = TRUE)
