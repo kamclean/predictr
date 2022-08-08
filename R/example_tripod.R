eda97e2471e3c8c9011509f205dc75d7bd43263e
kamclean <mcleankaca@gmail.com>
2022-08-08 15:29:41 +0100

    example tripod

---
+++ R/example_tripod.R
@@ -0,0 +1,11 @@
+# Documentation of datasets --------------------------------
+# example_tripod
+#' @docType data
+#' @title Example dataset of TRIPOD assessment
+#' @name example_tripod
+#' @aliases example_tripod
+#' @description Example dataset of TRIPOD assessment for use within vignettes
+#' @format A data frame with 36 rows and 39 variables.
+#' @usage data(example_tripod)
+#' @keywords data
+NULL
